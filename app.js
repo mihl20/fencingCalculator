60b7f2cbc96df5e21897afa9a39aab281098d640
mihl20 <mihl@hotmail.co.uk>
2023-06-09 15:57:54 +0100

    Add Code

---
+++ app.js
@@ -0,0 +1,30 @@
+function Calculate() {
+    var buttonValue = document.getElementById('btn').value;
+    if(document.getElementById("length").value === ""){
+      document.getElementById("label-description").innerHTML = "Please enter a length";
+      document.getElementById("label-description").style.color = "red";
+      return;
+    }
+    if(buttonValue === "calculate"){
+    var length = document.getElementById("length").value;
+    var lengthInFeet = length * 3.28084;
+    var rails = Math.ceil((length / 3.6) * 2);
+    var posts = Math.ceil((length / 1.8) + 1);
+    var postcrete = Math.ceil(posts * 2);
+    var pickets = Math.ceil(length / 0.125);
+      document.getElementById("label-description").innerHTML = "Fence Length (Meters)";
+      document.getElementById("label-description").style.color = "black";
+    document.getElementById("pickets").innerHTML = pickets;
+    document.getElementById("rails").innerHTML = rails;
+    document.getElementById("posts").innerHTML = posts;
+    document.getElementById("postcrete").innerHTML = postcrete;
+    document.getElementById("btn").value = "reset";
+    } else {
+      document.getElementById("length").value = "";
+      document.getElementById("pickets").innerHTML = "-";
+    document.getElementById("rails").innerHTML = "-";
+    document.getElementById("posts").innerHTML = "-";
+    document.getElementById("postcrete").innerHTML = "-";
+    document.getElementById("btn").value = "calculate";
+    }
+  }
