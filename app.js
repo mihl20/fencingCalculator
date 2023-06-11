316c55dc49b072f943e79eec619c606c34c9a1d2
mihl20 <mihl@hotmail.co.uk>
2023-06-11 21:25:13 +0100

    "Update entry method"

--- app.js
+++ app.js
@@ -8,9 +8,19 @@ function fenceSelection() {
 
 }
 
+const resetPicket = () => {
+  document.getElementById("length").value = "";
+  document.getElementById("pickets").value = "";
+  document.getElementById("rails").innerHTML = "-";
+  document.getElementById("posts").innerHTML = "-";
+  document.getElementById("postcrete").innerHTML = "-";
+  document.getElementById("btn").value = "calculate";
+}
+
 function goBack() {
   document.getElementById("picket3").style.display = "none";
   document.getElementById("fence-selector").style.display = "block";
+  resetPicket();
 }
 
 function Calculate() {
@@ -18,6 +28,7 @@ function Calculate() {
   if (document.getElementById("length").value === "") {
     document.getElementById("label-description").innerHTML = "Please enter a length";
     document.getElementById("label-description").style.color = "red";
+    document.getElementById("label-description").style.fontWeight = "600";
     return;
   }
   if (buttonValue === "calculate") {
@@ -28,18 +39,13 @@ function Calculate() {
     var postcrete = Math.ceil(posts * 2);
     var pickets = Math.ceil(length / 0.125);
     document.getElementById("label-description").innerHTML = "Fence Length (Meters)";
-    document.getElementById("label-description").style.color = "black";
-    document.getElementById("pickets").innerHTML = pickets;
+    document.getElementById("label-description").style.color = "white";
+    document.getElementById("pickets").value = pickets;
     document.getElementById("rails").innerHTML = rails;
     document.getElementById("posts").innerHTML = posts;
     document.getElementById("postcrete").innerHTML = postcrete;
     document.getElementById("btn").value = "reset";
   } else {
-    document.getElementById("length").value = "";
-    document.getElementById("pickets").innerHTML = "-";
-    document.getElementById("rails").innerHTML = "-";
-    document.getElementById("posts").innerHTML = "-";
-    document.getElementById("postcrete").innerHTML = "-";
-    document.getElementById("btn").value = "calculate";
+    resetPicket();
   }
 }
